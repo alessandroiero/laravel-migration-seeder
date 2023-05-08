224df55feddbb5f28ebef383ba1e8c86ffaef584
alessandroiero <alessandro.iero@outlook.it>
2023-05-08 11:19:32 +0200

    Stampati elementi + Page Controller where

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+
+        $trains = Train::where('orario_partenza', '>=', now('Europe/Rome'))->get();;
+
+        return view('home', compact('trains'));}
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,22 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Laravel Migration Seeder</title>
          @vite('resources/js/app.js')
     </head>
     <body>
         <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
+            <ul>
+                @foreach ($trains as $train)
+                <li>{{$train->azienda}}</li>
+                <li>{{$train->stazione_partenza}}</li>
+                <li>{{$train->stazione_arrivo}}</li>
+                <li>{{$train->orario_partenza}}</li>
+                <li>{{$train->orario_arrivo}}</li>
+                <li>{{$train->orario_arrivo}}</li>
+                <li>{{$train->n_carrozza}}</li>
+                @endforeach
+            </ul>
         </div>
-
     </body>
 </html>
